68e624d82e68c9248065dadb6c3d1f41cbb3770d
Eonist <30n1st@gmail.com>
2017-06-15 13:47:51 +0200

    'New files added: 1, Files modified: 1'
    'Added 1 file:
    log/utils/
    
    Modified 1 file:
    log/GitLogParser.swift
    '

--- log/GitLogParser.swift
+++ log/GitLogParser.swift
@@ -1,47 +1,7 @@
 import Foundation
-typealias CommitData = (hash:String,author:String,date:String,subject:String,body:String)/*convenience*/
+
 class GitLogParser {
     static var debugCounter:Int = 0
-    /**
-     * Returns a ComitData instance that is populated with auther, date, subject, body
-     * EXAMPLE: GitLogParser.commitData("Hash:4caecd \n Author:Eonist \n Date:2015-12-03 16:59:09 +0100 \n Subject:'abc' \n Body:'123'")//Output: a CommitData instance
-     * NOTE: the log data is formated using --pretty=format
-     */
-    static func commitData(_ input:String) -> CommitData{
-        //Swift.print("input: " + "\(input)")
-        let firstIdx:Int = input.indexOf("\n")
-        //Swift.print("firstIdx: " + "\(firstIdx)")
-        let firstPart:String = input.subString(0,firstIdx)
-        //Swift.print("firstPart: " + "\(firstPart)")
-        let secondIdx:Int = firstIdx+1 + input.subString(firstIdx+1,input.count).indexOf("\n")
-        //Swift.print("secondIdx: " + "\(secondIdx)")
-        let secondPart:String = input.subString(firstIdx+1,secondIdx)
-        //Swift.print("secondPart: " + "\(secondPart)")
-        let thirdIdx:Int = secondIdx+1 + input.subString(secondIdx+1,input.count).indexOf("\n")
-        //Swift.print("thirdIdx: " + "\(thirdIdx)")
-        let thirdPart:String = input.subString(secondIdx+1,thirdIdx)
-        //Swift.print("thirdPart: " + "\(thirdPart)")
-        let fourthIdx:Int = thirdIdx+1 + input.subString(thirdIdx+1,input.count).indexOf("\n")
-        //Swift.print("fourthIdx: " + "\(fourthIdx)")
-        let fourthPart:String = input.subString(thirdIdx+1,fourthIdx)
-        //Swift.print("fourthPart: " + "\(fourthPart)")
-        let fifthPart:String = input.subString(fourthIdx+1,input.count)
-        //Swift.print("fifthPart: " + "\(fifthPart)")
-        var commitData:CommitData = (firstPart,secondPart,thirdPart,fourthPart,fifthPart)
-        commitData.hash = commitData.hash.subString("Hash:".count, commitData.hash.count)
-        commitData.author = commitData.author.subString("Author:".count, commitData.author.count)
-        commitData.date = commitData.date.subString("Date:".count, commitData.date.count)
-        commitData.subject = commitData.subject.subString("Subject:".count, commitData.subject.count)
-        commitData.body = commitData.body.subString("Body:".count, commitData.body.count)
-        /*
-        Swift.print("commitData.hash: " + "\(commitData.hash)")
-        Swift.print("commitData.author: " + "\(commitData.author)")
-        Swift.print("commitData.date: " + "\(commitData.date)")
-        Swift.print("commitData.subject: " + "\(commitData.subject)")
-        Swift.print("commitData.body: " + "\(commitData.body)")
-        */
-        return commitData
-    }
     /**
      * Compacts the bodyStr aka the commit description
      */
